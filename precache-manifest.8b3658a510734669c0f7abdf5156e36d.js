41bc0c9e46a7a545b6b8b87f2b9e110d860875f2
Ward <wardv@tirf.ca>
2022-04-08 14:02:40 -0400

    Updates

---
+++ precache-manifest.8b3658a510734669c0f7abdf5156e36d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "892b46a1fbe5cbe2773a",
+    "url": "/photo-port/static/css/main.755cf1aa.chunk.css"
+  },
+  {
+    "revision": "892b46a1fbe5cbe2773a",
+    "url": "/photo-port/static/js/main.892b46a1.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "/photo-port/static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "e73dd056ae2f236e827a",
+    "url": "/photo-port/static/css/2.d3586f91.chunk.css"
+  },
+  {
+    "revision": "e73dd056ae2f236e827a",
+    "url": "/photo-port/static/js/2.e73dd056.chunk.js"
+  },
+  {
+    "revision": "a1ebc147513b31b9783c",
+    "url": "/photo-port/static/js/runtime~main.a1ebc147.js"
+  },
+  {
+    "revision": "2aa1243278c2e35a6ebf82ac2e0658fb",
+    "url": "/photo-port/static/media/Ward2.2aa12432.jpg"
+  },
+  {
+    "revision": "0f2fd2692a632712d9b65b29ff74e10b",
+    "url": "/photo-port/static/media/hero-bg.0f2fd269.jpg"
+  },
+  {
+    "revision": "59c1bee25f6bc50d6da12063cf00b196",
+    "url": "/photo-port/static/media/pink-floyd.59c1bee2.jpg"
+  },
+  {
+    "revision": "cff10df8cd6fa4edca64fffced14d29b",
+    "url": "/photo-port/static/media/run-girl.cff10df8.png"
+  },
+  {
+    "revision": "75d5e399629252e93160230cee236c84",
+    "url": "/photo-port/static/media/README-generator.75d5e399.jpg"
+  },
+  {
+    "revision": "366f91a813427441259aeee809099436",
+    "url": "/photo-port/static/media/lord-of-the-rinks.366f91a8.png"
+  },
+  {
+    "revision": "ed1cd11392065c3223d356e3fc292a62",
+    "url": "/photo-port/static/media/NoSQL.ed1cd113.jpg"
+  },
+  {
+    "revision": "a3ed716a123abe5b23f743b02c859ba5",
+    "url": "/photo-port/static/media/PWA-budget-tracker.a3ed716a.png"
+  },
+  {
+    "revision": "a4498b347e0362d36a0edf0fb9b406c1",
+    "url": "/photo-port/index.html"
+  }
+];
